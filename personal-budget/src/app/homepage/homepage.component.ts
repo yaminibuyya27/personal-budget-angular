ac168d07172a43551e94aab6e2fec03fddd87997
Yamini Buyya <yaminibuyya27@gmail.com>
2025-09-21 22:24:44 -0400

    Added Angular

---
+++ personal-budget/src/app/homepage/homepage.component.ts
@@ -0,0 +1,54 @@
+import { AfterViewInit, Component } from '@angular/core';
+import{ HttpClient } from '@angular/common/http';
+import{ Chart } from 'chart.js';
+
+@Component({
+  selector: 'pb-homepage',
+  templateUrl: './homepage.component.html',
+  styleUrls: ['./homepage.component.scss']
+})
+export class HomepageComponent implements AfterViewInit {
+
+public dataSource: any = {
+                datasets: [
+                    {
+                        data: [],
+                        backgroundColor: [
+                            '#ffcd56',
+                            '#ff6384',
+                            '#36a2eb',
+                            '#fd6b19',
+                        ]
+                    }
+                ],
+                labels: []
+            };
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  ngAfterViewInit(): void {
+    this.http.get('http://localhost:3000/budget')
+    .subscribe((res : any) => {
+       for (var i = 0; i < res.myBudget.length; i++) {
+                    this.dataSource.datasets[0].data[i] = res.myBudget[i].budget;
+                    this.dataSource.labels[i] = res.myBudget[i].title;
+                    this.createChart();
+
+
+       }
+
+    });
+
+  }
+
+  createChart() {
+            var ctx: any = document.getElementById('myChart');
+            var myPieChart = new Chart(ctx, {
+                type: 'pie',
+                data: this.dataSource
+            });
+          }
+
+}
